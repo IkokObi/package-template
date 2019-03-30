4015bf4801f0cd71f5e9780b5747b991dd059bad
Oohikata <koki.obi.321@gmail.com>
2019-03-30 15:48:35 +0900

    :wastebasket: Remove optional files

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
     url='https://github.com/IkokObi/package_template',
     install_requires=['numpy>=1.14'],
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    test_suite='tests'
 )
 
