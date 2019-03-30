7a2cdb1e19b8f00f4dc5641e1d21683251f833d5
Oohikata <koki.obi.321@gmail.com>
2019-03-30 17:13:56 +0900

    :sparkles: Rename package name

--- setup.py
+++ setup.py
@@ -6,11 +6,8 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
-    name='template',
+    name='package-template',
     version='0.0.1',
     description='Template package for Obinata',
     long_description=readme,
@@ -18,8 +15,6 @@ setup(
     author_email='koki.obi.321@gmail.com',
     url='https://github.com/IkokObi/package_template',
     install_requires=['numpy>=1.14'],
-    license=license,
     packages=find_packages(exclude=('tests', 'docs')),
     test_suite='tests'
 )
-
